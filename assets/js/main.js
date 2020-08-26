49b5ee27305dc2e4aeadeec0d51221c6ba81e66d
rifat mondol <rifatmondol791@gmail.com>
2020-08-26 23:32:12 +0600

    Delete main.js

--- assets/js/main.js
+++
@@ -1,8 +0,0 @@
-var x = document.querySelector("#mybtn");
-var y = document.querySelector(".fa-angle-up");
-var z = document.querySelector(".fa-angle-down");
-
-x.addEventListener("click", function(){
-	y.style.display = "none";
-	z.style.display = "block";
-});
